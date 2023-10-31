f4744faefc92a416afbfe1037ea350a8e32bdd1b
VictroNoriega <vnoriega@unicesar.edu.co>
2023-10-31 13:16:03 -0500

    31-10-2023

--- src/components/SideBag.jsx
+++ src/components/SideBag.jsx
@@ -1,13 +1,13 @@
 import {useState} from'react'
-
+import '../styles/styles.css'
 
 
 const SideBar = () => {
     const [open, setOpen] = useState(false)
     return (
-        <div className='bg-transparent py-10 flex top-0 right-0'>
-            <button className='mr-8' onClick={() => setOpen(true)}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <div  className='bg-transparent py-10 flex top-0 right-0'>
+            <button className='mr-4' onClick={() => setOpen(true)}>
+                <svg className='svg-size' xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                     <path d="M6.331 8h11.339a2 2 0 0 1 1.977 2.304l-1.255 8.152a3 3 0 0 1 -2.966 2.544h-6.852a3 3 0 0 1 -2.965 -2.544l-1.255 -8.152a2 2 0 0 1 1.977 -2.304z"></path>
                     <path d="M9 11v-5a3 3 0 0 1 6 0v5"></path>
